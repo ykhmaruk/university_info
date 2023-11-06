0f6175c34515da4e6ce1089c8128a55a8c20abdd
Yaroslav Khmaruk <yarik9656422@gmail.com>
2023-11-06 10:16:39 +0200

    added base services and fixed entities

---
+++ src/main/java/com/example/university_info/service/DepartmentService.java
@@ -0,0 +1,6 @@
+package com.example.university_info.service;
+
+import com.example.university_info.entity.Department;
+
+public interface DepartmentService extends AbstractService<Department>{
+}
---
+++ src/main/java/com/example/university_info/service/LectorService.java
@@ -0,0 +1,6 @@
+package com.example.university_info.service;
+
+import com.example.university_info.entity.Lector;
+
+public interface LectorService extends AbstractService<Lector>{
+}
